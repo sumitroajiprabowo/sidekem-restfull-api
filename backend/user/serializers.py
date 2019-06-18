ae33250179a2343cfe26eeddb45ae1b4a7497046
Spectre <bowo@anakdesa.id>
2019-06-18 14:15:01 +0700

    Add AuthTokenSerializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,7 +1,7 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.models import Group
-
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import serializers
 from core.models import UserProfile
 
 
@@ -35,3 +35,31 @@ class RegisterSerializer(serializers.ModelSerializer):
         for group_data in groups_data:
             user.groups.add(group_data)
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={
+            'input_type': 'password'
+        },
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _('Unable to authenticate with provide credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
