64e0064b578db0ee820bc305e9f4e4fcf4df9296
Spectre <bowo@anakdesa.id>
2019-06-18 14:15:23 +0700

    Add CreateTokenView

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,11 +1,9 @@
 from rest_framework import viewsets, generics
-from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from django.contrib.auth.models import Group
-
-from core.models import User
-from user.serializers import RegisterSerializer, GroupSerializer
-# Also add these imports
-from user.permissions import IsLoggedInUserOrAdmin, IsAdminUser
+from user.serializers import RegisterSerializer, GroupSerializer,\
+    AuthTokenSerializer
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -18,3 +16,9 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
